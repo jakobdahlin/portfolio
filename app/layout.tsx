a53923c36be175b2123dd789ed283cff4eb763f3
Dahlin301 <jakob.dahlin@gmail.com>
2025-02-12 21:43:06 -0500

    Updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,26 @@
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ChevronDown from "./components/ChevronDown/chevrondown";
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Jakob Dahlin Portfolio',
+  description: 'Web Design - Graphic Design & Architectural Photography',
+  openGraph: {
+    title: 'Jakob Dahlin Portfolio',
+    description: 'Web Design - Graphic Design & Architectural Photography',
+    images: [
+      {
+        url: 'https://jakobdahlin.vercel.app/logos/JDIDWi.png',
+        width: 1200,
+        height: 630,
+        alt: 'Jakob Dahlin Portfolio Preview'
+      }
+    ],
+    url: 'https://jakobdahlin.vercel.app',
+    type: 'website'
+  }
+}
 
 export default function RootLayout({
   children,
