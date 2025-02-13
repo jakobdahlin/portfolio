45f68cc121cb031604d8ae49ea1ec6e9273fd889
Dahlin301 <jakob.dahlin@gmail.com>
2025-02-12 19:33:40 -0500

    Resolved merge conflicts

--- app/components/welcomeintro/welcomeintro.tsx
+++ app/components/welcomeintro/welcomeintro.tsx
@@ -15,7 +15,7 @@ const Welcomecard: React.FC<CardProps> = ({ className }) => {
         <div className="w-full max-w-xl px-1">
             <h1 className="text-responsive font-bold text-white py-2">Jakob Dahlin</h1>
             <p className="tracking-responsive text-white 0 py-4">DESIGN PORTFOLIO</p>
-            <p className="my-12 mx-12">15+ years in graphic design and 10 years in web design, creating marketing materials for both digital and print. Specialized in developing user-friendly websites and web applications with focus on single-page experiences and affiliate marketing blogs. Skilled in UI design optimization to enhance SEO and improve UX. </p>
+            <p className="my-12 mx-10">15+ years in graphic design and 10 years in web design, creating marketing materials for both digital and print. Specialized in developing user-friendly websites and web applications with focus on single-page experiences and affiliate marketing blogs. Skilled in UI design optimization to enhance SEO and improve UX. </p>
           </div>
           <Contactbutton />
         </Backgroundcard>
