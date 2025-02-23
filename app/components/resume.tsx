c24545f94feddd6c76a1a30b009cf685bb618780
Dahlin301 <jakob.dahlin@gmail.com>
2025-02-22 23:14:53 -0500

    Resume updates

--- app/components/resume.tsx
+++ app/components/resume.tsx
@@ -19,8 +19,8 @@ const skills = {
 
 const experiences = [
   {
-    title: "Web Designer, SEO & Graphic Designer – Self-employed",
-    period: "2023 – Present",
+    title: "2023 - Present: Web Designer, SEO & Graphic Designer",
+    period: "Self-employed",
     location: "New York, NY (Remote)",
     description: [
       "I am expanding my knowledge in web design and UI/UX design, while integrating these skills into my creative work as an architectural photographer. This expansion allows me to create responsive, user-friendly websites that reflect both functionality and aesthetics.",
@@ -51,24 +51,24 @@ const experiences = [
     ],
   },
   {
-    title: "Architectural Photographer – Self-employed",
-    period: "2015 – Present",
+    title: "2015 - Present: Architectural Photographer",
+    period: "Self-employed",
     location: "Örebro, Sweden & New York, NY (Hybrid)",
     description: [
       "As an architectural photographer, I work in both residential and commercial real estate. I document each project from inception to completion, capturing every stage with close attention to detail that reflects my vision and architectural expertise.",
     ],
   },
   {
-    title: "Childcare Worker – Örebro Municipality",
-    period: "2014 – 2016",
+    title: "2014 - 2016: Childcare Worker",
+    period: "Örebro Municipality",
     location: "Örebro, Sweden (Onsite)",
     description: [
       "Organized and led age-appropriate activities for children, encouraged creativity and social development. Managed digital lists and documentation and creation of marketing material.",
     ],
   },
   {
-    title: "System Administrator – Örebro Municipality",
-    period: "2013 – 2014",
+    title: "2013 - 2014: System Administrator",
+    period: "Örebro Municipality",
     location: "Örebro, Sweden (Onsite)",
     description: [
       "Helped launch a project to connect job-seeking individuals with companies through a custom-built system. Managed website, social media, and marketing material for various events.",
@@ -78,14 +78,24 @@ const experiences = [
 
 const education = [
   {
-    title: "Military Service",
-    institution: "P4 Regiment, Skövde, Sweden",
-    period: "2009 – 2010",
+    title: "Swedish Armed Forces – Skaraborg Regiment (Försvarsmakten – Skaraborgs Regemente P4)",
+    major: "Military Service – Field Operations & Culinary Training",
+    degree: "Diploma & Service Medal",
+    location: "Skövde, Sweden, Karlskrona, Sweden",
+    description: [
+      "Completed 11 months of military service at P4 Skaraborgs Regiment, including general basic training, national field exercises, and a one-month culinary training program at a naval base in southern Sweden.",
+    ],
+    period: "2009 – 2010"
   },
   {
-    title: "Media & Communication",
-    institution: "Virginska Skolan, Örebro, Sweden",
-    period: "2006 – 2009",
+    title: "Virginska Skolan",
+    major: "Media Communication",
+    degree: "Upper Secondary Diploma (Gymnasieexamen)",
+    location: "Örebro, Sweden",
+    description: [
+      "Studied Media Communication with a focus on Photography, Graphic Design, and Video Production. Over three years, gradually specialized in Graphic Design and Video Production.",
+    ],
+    period: "2006 – 2009"
   },
 ]
 
@@ -129,9 +139,9 @@ export function Resume() {
           {/* Main Content */}
           <main className="space-y-6">
             <Collapsible title="Experience" defaultOpen={true} className="text-xl">
-              <div className="space-y-4">
+              <div className="space-y-4 text-lg">
                 {experiences.map((exp, index) => (
-                  <Collapsible key={index} title={exp.title} defaultOpen={false} >
+                  <Collapsible key={index} title={exp.title} defaultOpen={false}>
                     <div className="space-y-2">
                       <p className="text-sm text-netural-600">
                         {exp.period} – {exp.location}
@@ -165,9 +175,24 @@ export function Resume() {
                 {education.map((edu, index) => (
                   <div key={index} className="space-y-2">
                     <h3 className="text-lg font-semibold text-white">{edu.title}</h3>
-                    <p className="text-sm text-netural-600">
-                      {edu.institution} – {edu.period}
+                    <p className="text-sm text-white font-bold flex">
                     </p>
+                    <p className="text-sm text-white font-bold inline-flex">
+                        Major: <span className="text-sm text-neutral-600">{edu.major}</span>
+                    </p>
+                    <p className="text-sm text-white font-bold inline-flex">
+                        Degree: <span className="text-sm text-neutral-600">{edu.degree}</span>
+                    </p>
+                    <p className="text-sm text-white font-bold inline-flex">
+                        Location: <span className="text-sm text-neutral-600">{edu.location}</span>
+                    </p>
+                    <p className="text-sm text-white font-bold inline-flex">
+                        Description: <span className="text-sm text-neutral-600">{edu.description}</span>
+                    </p>
+                    <p className="text-sm text-white font-bold inline-flex">
+                        Period: <span className="text-sm text-neutral-600">{edu.period}</span>
+                    </p>
+
                   </div>
                 ))}
               </div>
